b02e0cd67f5ef114267a6b88b6ed606db5e53637
ParvPatel01 <patel.parv1@northeastern.edu>
2024-07-18 23:00:15 -0400

    updated Package.json

--- OpenFields/src/router.ts
+++ OpenFields/src/router.ts
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Market from "./components/Market";
 import Mint from "./components/Mint";
-import Profile from "./components/Profile";
 
 
 const routes = createBrowserRouter([
